86d25dc1404747aff109e2dfeb102dab274124ec
xuyang <1351267051@qq.com>
2026-05-27 10:50:05 +0800

    更新配置

--- shop/capacitor.config.ts
+++ shop/capacitor.config.ts
@@ -5,8 +5,7 @@ const config: CapacitorConfig = {
   appName: '文硕商城',
   webDir: 'dist',
   server: {
-    /** API 为 http://IP 时必须用 http，否则 WebView 会拦截 https 页面访问 http 接口 */
-    androidScheme: 'http',
+    androidScheme: 'https',
   },
 }
 
